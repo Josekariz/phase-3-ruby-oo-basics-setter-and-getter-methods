accc5d3cd97e47399113c882dbf620968c8349f6
Josekariz <joseph.macharia@student.moringaschool.com>
2022-10-21 10:06:39 +0300

    Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
 
+  # def name=(name)
+  #   @name = name
+  # end
+
+  # # getter method
+  # def name
+  #   @name
+  # end
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+
+  # # getter method
+  # def breed
+  #   @breed
+  # end
+attr_reader :name
+attr_reader:breed
+  def name=(name)
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
 end
